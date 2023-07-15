c4c6b25ee3f356ed6e79488485db8b02334c0111
Harshil0309 <harshilgupta562@gmail.com>
2023-07-16 02:26:05 +0530

    Project

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const server = express();
+const port = process.env.PORT || 5000;
+const dbConnection = require("./db");
+server.use(express.json())
+
+server.use('/api/cars',require('./routes/carsRoute'))
+server.use('/api/users',require('./routes/usersRoute'))
+server.use('/api/bookings/',require('./routes/bookingsRoute'))
+
+// const path = require('path')
+
+// if(process.env.NODE_ENV==='production')
+// {
+
+//     app.use('/' , express.static('client/build'))
+
+//     app.get('*' , (req , res)=>{
+
+//           res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+
+//     })
+
+// }
+
+server.get("/", (req, res) => res.send("hello"));
+server.listen(port, () => console.log(`Listening the server in ${port}`));
