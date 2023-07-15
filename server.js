d739e9531abeb8e31cff07912918421ea2c321d5
Harshil0309 <harshilgupta562@gmail.com>
2023-07-16 02:40:08 +0530

    Some Changes

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ server.use('/api/cars',require('./routes/carsRoute'))
 server.use('/api/users',require('./routes/usersRoute'))
 server.use('/api/bookings/',require('./routes/bookingsRoute'))
 
-// const path = require('path')
+const path = require('path')
 
-// if(process.env.NODE_ENV==='production')
-// {
+if(process.env.NODE_ENV==='production')
+{
 
-//     app.use('/' , express.static('client/build'))
+    app.use('/' , express.static('client/build'))
 
-//     app.get('*' , (req , res)=>{
+    app.get('*' , (req , res)=>{
 
-//           res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 
-//     })
+    })
 
-// }
+}
 
 server.get("/", (req, res) => res.send("hello"));
 server.listen(port, () => console.log(`Listening the server in ${port}`));
