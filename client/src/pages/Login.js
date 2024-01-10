032565222b0f3cdcf4a1958cf70b43641d995312
Harshil0309 <harshilgupta562@gmail.com>
2024-01-10 20:32:38 +0530

    Changes in Login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,32 +1,43 @@
 import React from "react";
 import { Row, Col, Form, Input } from "antd";
 import { Link } from "react-router-dom";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { userLogin } from "../redux/actions/userActions";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; 
-import Spinner from "../components/Spinner"
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Spinner from "../components/Spinner";
 AOS.init();
 function Login() {
-  const dispatch=useDispatch();
-  const {loading}=useSelector(state=>state.alertsReducer)
-  function onFinish(values){
-    dispatch(userLogin(values))
+  const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.alertsReducer);
+  function onFinish(values) {
+    dispatch(userLogin(values));
     // console.log(values);
   }
+  const inputStyle = {
+    width: "110%",
+    height: "110%",
+    backgroundColor: "#1F1F1F",
+    borderColor: "#1F1F1F",
+  };
   return (
     <div className="login">
-      {loading && (<Spinner/>)}
+      {loading && <Spinner />}
       <Row gutter={16} className="d-flex align-items-center">
         <Col lg={16} style={{ position: "relative" }}>
           <img
-           data-aos='slide-right'
-           data-aos-duration='1500'
-          src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" />
+            data-aos="slide-right"
+            data-aos-duration="1500"
+            src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+          />
           <h1 className="login-logo">Rent A Car</h1>
         </Col>
         <Col lg={8} className="text-left p-5">
-          <Form layout="vertical" className="login-form p-5" onFinish={onFinish}>
+          <Form
+            layout="vertical"
+            className="login-form p-5"
+            onFinish={onFinish}
+          >
             <h1>Login</h1>
             <hr />
             <Form.Item
@@ -41,7 +52,7 @@ function Login() {
               label="Password"
               rules={[{ required: true }]}
             >
-              <Input />
+              <Input.Password style={inputStyle} />
             </Form.Item>
             <button className="btn1 mt-2">Login</button>
             <br />
